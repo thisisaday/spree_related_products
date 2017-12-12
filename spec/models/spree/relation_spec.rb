2b69399d7d643ecf50ff69b14c2cbe77049b6c3f
Thomas von Deyen <thomas@vondeyen.com>
2017-12-12 17:30:12 +0100

    Touch products when relation gets created/updated
    When a product gets related to another product the cache key doesn't get
    updated. This fixes this by enabling touch option the belongs_to
    associations.

--- spec/models/spree/relation_spec.rb
+++ spec/models/spree/relation_spec.rb
@@ -12,4 +12,20 @@ describe Spree::Relation do
     it { should validate_presence_of(:relatable) }
     it { should validate_presence_of(:related_to) }
   end
+
+  describe 'after create' do
+    let!(:product) { create(:product) }
+    let!(:related_product) { create(:product) }
+
+    it 'touches the products', :aggregate_failures do
+      expect(product).to receive(:touch)
+      expect(related_product).to receive(:touch)
+
+      Spree::Relation.create!(
+        relation_type: create(:relation_type),
+        relatable: product,
+        related_to: related_product
+      )
+    end
+  end
 end
